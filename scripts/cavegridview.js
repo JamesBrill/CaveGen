48ac130e04aa95a4d5c04ab57fc3d5182437a26b
James Brill <jebssnooker@hotmail.com>
2015-01-10 22:20:31 +0000

    Made CaveGridView size customisable and added dummy mousedown event handler

--- scripts/cavegridview.js
+++ scripts/cavegridview.js
@@ -1,33 +1,48 @@
-var CaveGridView = function()
+var CaveGridView = function(x, y)
 {
 	this.location = {x:0, y:0};
-	this.pixelWidth = 800;
-	this.pixelHeight = 500;
-	this.width = 80;
-	this.height = 50;
-	this.tileSize = 10;
+	this.tileSize = 20;
+	this.pixelWidth = x * this.tileSize;
+	this.pixelHeight = y * this.tileSize;
+	this.width = x;
+	this.height = y;
 	this.canvas = document.getElementById("canvas");
+	this.canvas.width = this.pixelWidth;
+	this.canvas.height = this.pixelHeight;
 	this.context = canvas.getContext("2d");
 }
 
-CaveGridView.prototype.init = function()
+CaveGridView.prototype.draw = function(gridModel)
 {
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	for (var i = 0; i < this.width; i++)
 	{
 		for (var j = 0; j < this.height; j++)
 		{
-			this.drawAtCoordinates(i * this.tileSize, j * this.tileSize, "terrain");
+			this.drawAtGridCoordinates(i, j, gridModel[i][j]);
 		}
 	}
 }
 
-CaveGridView.prototype.drawAtCoordinates = function(x, y, tile)
+CaveGridView.prototype.drawAtGridCoordinates = function(x, y, tile)
 {
+	var view = this;
+	this.context.clearRect(x * view.tileSize, y * view.tileSize, view.tileSize, view.tileSize);
 	var image = new Image();
 	var context = this.context;
 	image.onload = function()
 	{
-		context.drawImage(image, x, y, 10, 10);
+		context.drawImage(image, x * view.tileSize, y * view.tileSize, view.tileSize, view.tileSize);
 	}
 	image.src = "images/" + tile + ".png";
+}
+
+CaveGridView.prototype.getGridX = function(pixelX)
+{
+	return ((pixelX - (pixelX % this.tileSize)) / this.tileSize);   
+}
+
+CaveGridView.prototype.getGridY = function(pixelY)
+{
+	return ((pixelY - (pixelY % this.tileSize)) / this.tileSize);   
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,45 @@
 $(document).ready(function () {  
-	var view = new CaveGridView();
-	view.init();   
+	var width = 40;
+	var height = 40;
+	var grid;
+	var view;
+
+	var init = function()
+	{
+		grid = createGrid(width, height);
+		view = new CaveGridView(width, height);
+		view.draw(grid); 
+		addEventListeners();
+	}
+
+	var addEventListeners = function()
+	{
+		view.canvas.addEventListener("mousedown", function (event) 
+		{
+	        var pixelX = event.pageX - this.offsetLeft;
+	        var pixelY = event.pageY - this.offsetTop;
+	        var gridX = view.getGridX(pixelX);
+	        var gridY = view.getGridY(pixelY);
+	        grid[gridY][gridX] = "heart";
+	        view.drawAtGridCoordinates(gridX, gridY, "heart");
+	    });
+	}
+
+	var createGrid = function(x, y)
+	{
+		var grid = new Array(x);
+		for (var i = 0; i < x; i++) {
+			grid[i] = new Array(y);
+		}
+		for (var i = 0; i < x; i++)
+		{
+			for (var j = 0; j < y; j++)
+			{
+				grid[i][j] = "terrain";
+			}
+		}
+		return grid;
+	}
+
+	init();
 });
