bfb6cd2f4ce4afad17b0388187d74a31db3d3975
James Brill <jebssnooker@hotmail.com>
2015-04-12 18:48:37 +0100

    GenerateCaveChange now implements imaginary CaveChange interface

--- static/scripts/generate-cave-change.js
+++ static/scripts/generate-cave-change.js
@@ -1,6 +1,58 @@
 function GenerateCaveChange(preGenerationSnapshot, postGenerationWidth, postGenerationHeight) 
 {
 	this.preGenerationSnapshot = preGenerationSnapshot;
+	this.preGenerationWidth = preGenerationSnapshot.length;
+	this.preGenerationHeight = preGenerationSnapshot[0].length;
 	this.postGenerationWidth = postGenerationWidth;
 	this.postGenerationHeight = postGenerationHeight;
+}
+
+// Returns true if regeneration changes the existing cave 
+GenerateCaveChange.prototype.hasEffect = function()
+{
+	if (this.preGenerationWidth != this.postGenerationWidth || 
+		this.preGenerationHeight != this.postGenerationHeight)
+	{
+		return true;
+	}
+
+	for (var i = 0; i < this.preGenerationWidth; i++) 
+	{
+		for (var i = 0; i < this.preGenerationHeight; i++) 
+		{
+			if (this.preGenerationSnapshot[i][j].symbol != 'x')
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+GenerateCaveChange.prototype.equals = function(other)
+{
+    if (!other)
+    {
+        return false;
+    }
+
+	if (this.preGenerationWidth != other.preGenerationWidth || 
+		this.preGenerationHeight != other.preGenerationHeight ||
+		this.postGenerationWidth != other.postGenerationWidth || 
+		this.postGenerationHeight != other.postGenerationHeight)
+	{
+		return false;
+	}
+
+	for (var i = 0; i < this.preGenerationWidth; i++) 
+	{
+		for (var i = 0; i < this.preGenerationHeight; i++) 
+		{
+			if (this.preGenerationSnapshot[i][j] != other.preGenerationSnapshot[i][j])
+			{
+				return false;
+			}
+		}
+	}
+    return true;
 }
