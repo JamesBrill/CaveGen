3842b6d0c7876293926226dc2766ba00520f3c04
James Brill <jebssnooker@hotmail.com>
2015-04-12 20:46:39 +0100

    Add utility methods for getting a border and tile size for a given cave size

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -6,3 +6,28 @@ var twoDimensionalArray = function(x, y)
 	}
 	return grid;
 }
+
+var getBorder = function(caveWidth, caveHeight)
+{
+	var widthHeightRatio = caveWidth / caveHeight;	
+	var border;
+	if (widthHeightRatio > 1)
+	{
+		var displayWidth = CAVE_DISPLAY_SIZE / widthHeightRatio;
+		var borderThickness = (CAVE_DISPLAY_SIZE - displayWidth) / 2;
+		border = { top: borderThickness, left: 0 };
+	}
+	else
+	{
+		var displayHeight = CAVE_DISPLAY_SIZE * widthHeightRatio;
+		var borderThickness = (CAVE_DISPLAY_SIZE - displayHeight) / 2;
+		border = { top: 0, left: borderThickness };
+	}
+	return border;
+}
+
+var getTileSize = function(caveWidth, caveHeight)
+{
+	var largestDimension = Math.max(caveWidth, caveHeight);
+	return CAVE_DISPLAY_SIZE / largestDimension; 
+}
