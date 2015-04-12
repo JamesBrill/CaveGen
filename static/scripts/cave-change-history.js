deb55c495121db2f16744c727bb0ff46876d049a
James Brill <jebssnooker@hotmail.com>
2015-04-12 20:45:50 +0100

    Add methods to move current change in history without adding a new change

--- static/scripts/cave-change-history.js
+++ static/scripts/cave-change-history.js
@@ -8,6 +8,16 @@ CaveChangeHistory.prototype.numberOfChanges = function() { return this.changes.l
 
 CaveChangeHistory.prototype.currentChange = function() { return this.changes[this.currentChangeIndex]; }
 
+CaveChangeHistory.prototype.rollBackCurrentChange = function() 
+{ 
+    this.currentChangeIndex = Math.max(0, this.currentChangeIndex - 1); 
+}
+
+CaveChangeHistory.prototype.rollForwardCurrentChange = function() 
+{ 
+    this.currentChangeIndex = Math.min(this.changes.length - 1, this.currentChangeIndex + 1); 
+}
+
 CaveChangeHistory.prototype.getTileChanges = function(index) { return this.changes[index].tileChanges; }
 
 CaveChangeHistory.prototype.isLastChangeARegeneration = function()
