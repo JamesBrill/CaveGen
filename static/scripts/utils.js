e5f9f655a3a721846cb6ac6cca01994cf0ce6018
JamesBrill <james.brill@red-gate.com>
2015-04-15 18:09:45 +0100

    Use more accurate names for new display dimensions

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -13,14 +13,14 @@ var getBorder = function(caveWidth, caveHeight)
 	var border;
 	if (widthHeightRatio > 1)
 	{
-		var displayWidth = CAVE_DISPLAY_SIZE / widthHeightRatio;
-		var borderThickness = (CAVE_DISPLAY_SIZE - displayWidth) / 2;
+		var displayHeight = CAVE_DISPLAY_SIZE / widthHeightRatio;
+		var borderThickness = (CAVE_DISPLAY_SIZE - displayHeight) / 2;
 		border = { top: borderThickness, left: 0 };
 	}
 	else
 	{
-		var displayHeight = CAVE_DISPLAY_SIZE * widthHeightRatio;
-		var borderThickness = (CAVE_DISPLAY_SIZE - displayHeight) / 2;
+		var displayWidth = CAVE_DISPLAY_SIZE * widthHeightRatio;
+		var borderThickness = (CAVE_DISPLAY_SIZE - displayWidth) / 2;
 		border = { top: 0, left: borderThickness };
 	}
 	return border;
