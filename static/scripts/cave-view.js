12d80428ea5673095e0a0e4205da2b5027d7324b
JamesBrill <james.brill@red-gate.com>
2015-04-15 17:56:29 +0100

    Use Bresenham's algorithm to draw line-grid without anti-aliasing

--- static/scripts/cave-view.js
+++ static/scripts/cave-view.js
@@ -13,6 +13,11 @@ var CaveView = function(x, y, tileSize, border)
 	this.context = this.canvas.getContext("2d");
 	this.paintLineMode = false;
 	this.isMouseDown = false;
+	this.whitePixelId = this.context.createImageData(1, 1);
+	this.whitePixelId.data[0] = 255;
+	this.whitePixelId.data[1] = 255;
+	this.whitePixelId.data[2] = 255;
+	this.whitePixelId.data[3] = 255;
 }
 
 CaveView.prototype.draw = function(gridModel)
@@ -21,6 +26,7 @@ CaveView.prototype.draw = function(gridModel)
 	this.context.rect(0, 0, this.canvas.width, this.canvas.height);
 	this.context.fillStyle = 'black';
 	this.context.fill();
+	this.drawLines();
 	for (var i = 0; i < this.width; i++)
 	{
 		for (var j = 0; j < this.height; j++)
@@ -28,12 +34,29 @@ CaveView.prototype.draw = function(gridModel)
 			this.drawAtGridCoordinates(i, j, gridModel.getTileAtCoordinates(i, j));
 		}
 	}
+
+}
+
+CaveView.prototype.drawLines = function()
+{
+	var view = this;
+	var offset = view.tileSize;
+	for (var i = 1; i < this.width; i++) 
+	{
+		var x = i * view.tileSize + view.border.left;
+		view.plotLine(x, view.border.top + offset, x, view.height * view.tileSize + view.border.top - offset)
+	}
+
+	for (var i = 1; i < this.height; i++) 
+	{
+		var y = i * view.tileSize + view.border.top;
+		view.plotLine(view.border.left + offset, y, view.width * view.tileSize + view.border.left - offset, y);
+	}
 }
 
 CaveView.prototype.drawAtGridCoordinates = function(x, y, tile)
 {
 	var view = this;
-	this.context.clearRect(x * view.tileSize + view.border.left, y * view.tileSize + view.border.top, view.tileSize, view.tileSize);
 	var left = x * view.tileSize + view.border.left;
 	var top = y * view.tileSize + view.border.top;
 	var size = view.tileSize;
@@ -113,4 +136,40 @@ CaveView.prototype.paintPositions = function(paintedPositions)
 	{
 		this.drawAtGridCoordinates(paintedPositions[i].x, paintedPositions[i].y, paintedPositions[i].brush);
 	}
+}
+
+CaveView.prototype.plotLine = function(x0, y0, x1, y1)
+{
+	var dx =  Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+	var dy = -Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+	var err = dx + dy, e2;                                   
+
+	for (;;)
+	{                                                          
+		this.setPixel(x0, y0);
+		if ((x0 == x1 && y0 == y1) || this.outOfBounds(x0, y0)) break;
+		e2 = 2 * err;
+		if (e2 >= dy) 
+		{ 
+			err += dy; 
+			x0 += sx; 
+		}                        
+		if (e2 <= dx) 
+		{ 
+			err += dx;
+			y0 += sy; 
+		}                        
+	}
+}
+
+CaveView.prototype.outOfBounds = function(x, y)
+{
+	var xLimit = (this.width - 1) * this.tileSize + this.border.left;
+	var yLimit = (this.height - 1) * this.tileSize + this.border.top;
+	return (x < 0 || y < 0 || x > xLimit || y > yLimit);
+}
+
+CaveView.prototype.setPixel = function(x, y)
+{
+	this.context.putImageData(this.whitePixelId, x, y);
 }
